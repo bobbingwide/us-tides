12c453986136503c4d446f228c0e0ec1eaa71366
Herb Miller <herb@bobbingwide.com>
2015-12-21 21:25:39 +0000

    Fixes #1 - Don't display SimpleXMLElement object text

--- shortcodes/us-tides.php
+++ shortcodes/us-tides.php
@@ -1,7 +1,7 @@
 <?php 
 /*
 
-    Copyright 2012, 2013 Bobbing Wide (email : herb@bobbingwide.com )
+    Copyright 2012-2015 Bobbing Wide (email : herb@bobbingwide.com )
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License version 2,
@@ -34,7 +34,8 @@
   */
 function us_get_tide_info( $tide_url ) {
   $request_url = urlencode( $tide_url );
-  $response_xml = simplexml_load_file($request_url);
+	bw_trace2( $request_url, "request_url", false );
+  $response_xml = simplexml_load_file( $request_url );
   bw_trace2( $response_xml );
   return $response_xml;
 }
@@ -127,63 +128,118 @@ function us_get_todays_date( $response ) {
 }
 
 /**
- * Form an URL for the given location assuming UK based
+ * Form an URL for the given location
+ * 
  * @param string $station - the station ID
  * @return string URL to use to load the XML file
  
  * 8675843 is Threemile Cut Entrance, Darien River, GA
  * 8423745 is Portsmouth, New Hampshire
  * @link http://tidesandcurrents.noaa.gov/noaatidepredictions/NOAATidesFacade.jsp?Stationid=8423745&datatype=Annual%20XML
+ 
+` 
+
+http://tidesandcurrents.noaa.gov/noaatidepredictions/NOAATidesFacade.jsp?datatype=Annual+XML&Stationid=8675843
+
+&text=datafiles%252F8675843%252F21122015%252F213%252F
+&imagename=images%2F8675843%2F21122015%2F213%2F8675843_2015-12-22.gif
+&bdate=20151221
+&timelength=daily
+&timeZone=2
+&dataUnits=1
+&interval=&edate=20151222
+&StationName=Threemile+Cut+entrance%2C+Darien+River
+&Stationid_=8675843
+&state=GA
+&primary=Subordinate
+&datum=MLLW
+&timeUnits=2
+&ReferenceStationName=Fort+Pulaski&ReferenceStation=8670870
+&HeightOffsetLow=*1.05
+
+&HeightOffsetHigh=*+1.03
+&TimeOffsetLow=55
+&TimeOffsetHigh=44
+&pageview=dayly
+&print_download=true
+&Threshold=&thresholdvalue=
+`
  */
 function us_tideurl_namify( $station) {
   $newurl = "http://tidesandcurrents.noaa.gov/noaatidepredictions/NOAATidesFacade.jsp?datatype=Annual XML&Stationid=" . $station ;
   return $newurl;
 }
 
+/**
+ * Return the name of the XML file
+ *
+ * @param string $station The Station ID
+ * @return string The XML file name. Now has a longer extension than before.
+ */
+function us_xml_file( $station="8423745" ) {
+	$xml = "Annual XML.Annual XML.Annual XML";
+  $file = oik_path( "xml/$station.$xml", "us-tides" );
+	return( $file );
+}
+
+/**
+ * Load the XML file 
+ *
+ * @param string $station Station ID
+ * @return $response The XML file with information for the current date
+ */
 function us_load_xml_file( $station="8423745" ) {  
-  $file = oik_path( "xml/$station.xml", "us-tides" );
-  $load_file = file_exists( $file ); 
-  if ( $load_file ) {
-    $response = simplexml_load_file( $file );
-    // Need to check the year 
-    $today = bw_format_date( null, "Ymd h:i" );
-      bw_trace2( $response->EndDate, "EndDate" );
-      bw_trace2( $today, "TODAY" );
-    if ( $response->EndDate <= $today ) {
-      $load_file = false;
-    }  
-    
-  } 
-  if ( !$load_file ) {
-    $tideurl = us_tideurl_namify( $station ); 
-    $response = us_get_tide_info( $tideurl );
-    if ( $response ) {
-    
-      //p( $response->EndDate . "from new file" ); 
-      $bytes = $response->asXml( $file );
-      bw_trace2( $bytes, "$file written", false );
-      // p( $bytes );
-    }    
-  }
-  return( $response );  
-}  
+	$file = us_xml_file( $station );
+	$load_file = file_exists( $file ); 
+	if ( $load_file ) {
+		$response = simplexml_load_file( $file );
+		// Need to check the year 
+		$today = bw_format_date( null, "Ymd h:i" );
+		bw_trace2( $response->EndDate, "EndDate" );
+		bw_trace2( $today, "TODAY" );
+		if ( $response->EndDate <= $today ) {
+			$load_file = false;
+		}  
+	} 
+	if ( !$load_file ) {
+		$tideurl = us_tideurl_namify( $station ); 
+		$response = us_get_tide_info( $tideurl );
+		if ( $response ) {
+			us_save_xml_file( $response, $file );
+		}    
+	}
+	return( $response );  
+} 
 
-
+/**
+ * Save the XML object as a local file
+ *
+ * @param object $reponse XML object
+ * @param string $file fully qualified file name
+ */
+function us_save_xml_file( $response, $file ) {
+	bw_trace2( null, null, true, BW_TRACE_VERBOSE );
+  bw_trace2( $response->EndDate, "EndDate from new file", false, BW_TRACE_VERBOSE ); 
+  $bytes = $response->asXml( $file );
+  bw_trace2( $bytes, "$file written", false, BW_TRACE_VERBOSE );
+} 
 
 /**
  * Display information about high and low tides obtained from the NOAA Tides and Currents website
- * 
- * @param array $atts- shortcode parameters
- * @return string Result of shortcode
- * @link http://tidesandcurrents.noaa.gov/noaatidepredictions
  *
  * e.g.
+ * `
  *   Threemile Cut entrance, Darien River GA
  *   May 18,2012
  *   02:10 AM 0.7 Low Tide
  *   07:50 AM 6.6 High Tide
  *   02:11 PM 0.2 Low Tide
  *   08:07 PM 7.8 High Tide
+ * `
+ *
+ * @param array $atts- shortcode parameters
+ * @return string Result of shortcode
+ *
  * 
  * 
  */                                                                        
@@ -210,21 +266,36 @@ function us_tides( $atts=NULL ) {
     $today = bw_format_date( null, "Y/m/d" );
     foreach ( $response->data->item as $item ) {
       if ( $item->date == $today ) {
-        sp( "item" );
-        sepan( "time" , $item->time );
-        e( "&nbsp;" );
-        sepan( "ft", $item->predictions_in_ft );
-        e( "&nbsp;" );
-        if ( $item->highlow == 'L' ) {
-          sepan( "highlow low", "Low Tide" );
-        } else {
-          sepan( "highlow high", "High Tide" );
-        }    
-        ep();
+				us_format_item( $item  );
       }  
     }
   }
   return( bw_ret());
+}
+
+/**
+ * Format a tide item
+ * 
+ * Note: We need to cast the item values to string to avoid getting e.g. 
+ * `SimpleXMLElement Object ( [0] => 12:46 AM ) `
+ * when we just want 12:46 AM
+ *
+ *
+ * @param SimpleXMLElement $item
+ */
+function us_format_item( $item  ) {
+	bw_trace2( $item, "item", false, BW_TRACE_DEBUG );
+	sp( "item" );
+	sepan( "time", (string) $item->time );
+	e( "&nbsp;" );
+	sepan( "ft", (string) $item->predictions_in_ft );
+	e( "&nbsp;" );
+	if ( $item->highlow == 'L' ) {
+		sepan( "highlow low", "Low Tide" );
+	} else {
+		sepan( "highlow high", "High Tide" );
+	}    
+	ep();
 }
 
 function us_tides__help( $shortcode='us_tides' ) {
@@ -244,5 +315,17 @@ function us_tides__example( $shortcode='us_tides' ) {
   $example = 'station=8423745';
   bw_invoke_shortcode( $shortcode, $example, $text );
   p( "The information displayed comes from the NOAA Tides and Currents website." );
+}
 
+/**
+ * List the station IDs
+ * http://tidesandcurrents.noaa.gov/tide_predictions.html
+ *
+ * @link http://tidesandcurrents.noaa.gov/noaatidepredictions
+ */
+function us_station_ids() {
+	// TBC;
+	$station_ids = array();
+	$station_ids = apply_filters( "us_tides_stations", $station_ids );
+	return( $station_ids );
 }
